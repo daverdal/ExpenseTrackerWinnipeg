1d7eac48700d722b2ab4eb2f4c92e434c126d89e
Dave <daverdal@gmail.com>
2018-02-10 20:19:13 -0600

    RE ran the scaffold generation for Wards because they were deleted.

--- app/models/ward.rb
+++ app/models/ward.rb
@@ -1,3 +1,2 @@
 class Ward < ApplicationRecord
-  has_many :expenses
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+=begin
+1000.times do
+  Expense.create{
+        wardId: Faker::Commerce.unique.product_name,
+        journalDate: Faker::Date,
+        vendor: Faker::StarWars,
+        description: "#{Faker::Commerce.material}.",
+        account: Faker::Number.digit()),
+        amount: Faker::Commerce.price
+  }
+end
+=end
+
+t.integer :wardId
+t.datetime :journalDate
+t.string :vendor
+t.integer :expenseType
+t.text :description
+t.integer :account
+t.decimal :amount
