bea504dad64962c8088494055e40aefd48c1fefc
Dave <daverdal@gmail.com>
2018-02-15 17:54:32 -0600

    Generated ExpenseTypes controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   resources :about
   resources :wards
   resources :expenses
-  resources :expense_type
+  resources :expense_types
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
