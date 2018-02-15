a1cfd987fd06242db90e877b51c2074910dffccd
Dave <daverdal@gmail.com>
2018-02-15 17:31:00 -0600

    Generated contoller and views for the ExpenseType class

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,25 @@
 Rails.application.routes.draw do
+  get 'expense_type/new'
+
+  get 'expense_type/create'
+
+  get 'expense_type/update'
+
+  get 'expense_type/edit'
+
+  get 'expense_type/destroy'
+
+  get 'expense_type/show'
+
+  get 'expense_type/index'
+
+  root to: 'expenses#index'
   get 'about/index'
   resources :about
   resources :wards
   resources :expenses
+  resources :expense_type
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
